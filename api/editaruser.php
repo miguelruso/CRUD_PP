bef74c474099a53ee85bd52db49053128a1d22e3
Lic. Miguel Berezosky <miguelruso@hotmail.es>
2023-11-02 23:45:50 -0300

    Update editaruser.php

--- api/editaruser.php
+++ api/editaruser.php
@@ -7,7 +7,7 @@ header("Allow: GET, POST, OPTIONS, PUT, DELETE");
 require "conexion.php";
     $json = file_get_contents("php://input");
     $objAfiliado = json_decode($json);
-    $sql = "UPDATE users SET user='$objAfiliado->nombre', '$objAfiliado->apellido', '$objAfiliado->user', password='$objAfiliado->password', email='$objAfiliado->email', level='$objAfiliado->level' WHERE idUser='$objAfiliado->idUser'";
+    $sql = "UPDATE users SET nombre='$objAfiliado->nombre', apellido='$objAfiliado->apellido', user='$objAfiliado->user', password='$objAfiliado->password', email='$objAfiliado->email', level='$objAfiliado->level' WHERE idUser='$objAfiliado->idUser'";
     $query = $mysqli->query($sql);
     $jsonRespuesta = array('msg' => 'OK');
     echo json_encode($jsonRespuesta);
